9c3e8405647214d0036a4a726e98ae3669ca7373
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2021-07-22 10:36:57 +0700

    20: Rough Prototype part 2

---
+++ src/main/java/C01_04_ioc_xmlConfig/TrackCoach.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package C01_04_ioc_xmlConfig;
+
+/**
+ *
+ * @author huynq
+ */
+public class TrackCoach implements Coach {
+
+    public TrackCoach() {
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Run a hard 5k";
+    }
+
+}
