e3b2d24bb9f8200e4134a5f9a28a24e09b89517b
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2021-07-22 09:28:42 +0700

    set up pom.xml, get version of springframework using SpringVersion.getVersion()

--- src/main/java/com/mycompany/mavenproject/spring/App.java
+++ src/main/java/com/mycompany/mavenproject/spring/App.java
@@ -1,13 +1,15 @@
 package com.mycompany.mavenproject.spring;
 
+import org.springframework.core.SpringVersion;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( SpringVersion.getVersion() );
     }
 }
